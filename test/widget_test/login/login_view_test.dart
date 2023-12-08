dc457433267b5bf834beb735081f570a9c348a34
BXBZwe <Legendaryzwe@gmail.com>
2023-12-08 11:40:48 +0700

    login_view_test fixed

--- test/widget_test/login/login_view_test.dart
+++ test/widget_test/login/login_view_test.dart
@@ -1,72 +1,61 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
-//import 'package:demo_provider_mvvm/src/modules/login/login_view.dart';
-//import 'package:demo_provider_mvvm/src/modules/home/home_view.dart';
-import '../../mocks.mocks.dart';
-import 'package:get_it/get_it.dart';
-//import 'package:demo_provider_mvvm/src/modules/login/login_view_model.dart';
+import 'package:demo_provider_mvvm/src/modules/login/login_view.dart';
+import '../../mocks.mocks.dart'; // Import your mocks
 import 'package:demo_provider_mvvm/src/utils/auth.dart';
-//import 'package:demo_provider_mvvm/src/data/models/user/user_model.dart';
-//import 'package:demo_provider_mvvm/src/data/services/user_service.dart';
-//import 'package:demo_provider_mvvm/src/routes.dart';
-//import 'package:demo_provider_mvvm/src/app.dart';
-import 'package:demo_provider_mvvm/src/common/app_config.dart';
-import 'package:demo_provider_mvvm/src/modules/profile/profile_view.dart';
+import 'package:demo_provider_mvvm/src/locator.dart';
+//import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
+//import 'package:mockito/mockito.dart';
+//import 'package:path_provider/path_provider.dart' as path_provider;
+//import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 
-class MockNavigatorObserver extends Mock implements NavigatorObserver {}
+//class MockPathProviderPlatform extends Mock implements PathProviderPlatform {}
 
 void main() {
   late MockAuthProvider mockAuthProvider;
-  late MockThemeManager mockThemeManager;
 
-  setUp(() {
+  setUp(() async {
+    //PathProviderPlatform.instance = MockPathProviderPlatform();
+
+    Directory tempDir = Directory.current;
+    Hive.init(tempDir.path);
     mockAuthProvider = MockAuthProvider();
-    mockThemeManager = MockThemeManager();
-
-    // Mock the behaviors of AuthProvider and ThemeManager
-    // (Your existing mock setups)
+    setupLocator();
   });
 
-  testWidgets(
-      'ProfileView should display user information, logout button, and support theme toggle',
+  testWidgets('LoginView will be displaying the home page.',
       (WidgetTester tester) async {
-    // Set initial theme to light mode
-    when(mockThemeManager.toggle).thenReturn(false);
-
-    final mockObserver = MockNavigatorObserver();
-    when(mockAuthProvider.setLoginStatus(any)).thenAnswer((_) async {});
-    when(mockAuthProvider.isLoggedIn).thenReturn(false);
     await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider<AuthProvider>.value(value: mockAuthProvider),
-          ChangeNotifierProvider<ThemeManager>.value(value: mockThemeManager),
-        ],
-        child: MaterialApp(
-          home: ProfileView(),
-          navigatorObservers: [mockObserver],
+      MaterialApp(
+        home: ChangeNotifierProvider<AuthProvider>(
+          create: (_) => mockAuthProvider,
+          child: LoginView(),
         ),
       ),
     );
 
-    // Verify initial state (light mode)
-    // (Your existing verifications)
-    expect(find.text('John Doe'), findsOneWidget);
-    expect(find.text('Software Developer'), findsOneWidget);
-    expect(find.byType(CircleAvatar), findsOneWidget);
-    expect(find.byType(ElevatedButton), findsOneWidget);
-    // Simulate theme change to dark mode
-    when(mockThemeManager.toggle).thenReturn(true);
-    // You need to trigger a rebuild of the widget after changing the theme
-    mockThemeManager.notifyListeners();
     await tester.pumpAndSettle();
-    verify(mockAuthProvider.setLoginStatus(false)).called(1);
-    expect(mockAuthProvider.isLoggedIn, isFalse);
+    await tester.pump(Duration(seconds: 2));
+
+    expect(find.byType(LoginView), findsOneWidget);
+
+    // Simulate user input
+    await tester.enterText(find.byKey(Key('Username')), 'BXBZwe');
+    await tester.enterText(find.byKey(Key('Password')), 'BXBZwe136928');
+
+    // Simulate login button tap
+    final Finder loginButton = find.byKey(Key('Auth'));
+    await tester.tap(loginButton);
+
+    await tester.pump(Duration(seconds: 2));
   });
 
-  tearDownAll(() {
-    GetIt.instance.reset();
+  tearDownAll(() async {
+    //GetIt.instance.reset();
+    await Hive.close();
+    //PathProviderPlatform.instance = null;
   });
 }
